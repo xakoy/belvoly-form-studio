eef442aae398e9aa3c4de48e69d4bf74d07e3682
xakoy <xakoy@qq.com>
2020-01-12 22:52:22 +0800

    divider组件增加属性和显示效果

---
+++ src/components/controls/divider/PropertyEditor.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <color :control="control"></color>
+        <padding :control="control"></padding>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Padding, Color } from '../props'
+
+export default Vue.extend({
+    props: ['control'],
+    components: {
+        Color,
+        Padding
+    }
+})
+</script>
--- src/components/controls/divider/index.ts
+++ src/components/controls/divider/index.ts
@@ -1,18 +1,25 @@
 import { Config, IControl } from '../config'
 import Component from './Index.vue'
+import Editor from './PropertyEditor.vue'
 
 const config: Config = {
     name: 'divider',
     text: '分割线',
     icon: 'sidebar_hr',
     prop: {
-        label: '分割线'
+        label: '分割线',
+        color: '',
+        padding: {
+            top: 10,
+            bottom: 10
+        }
     }
 }
 
 const control: IControl = {
     config: config,
-    component: Component
+    component: Component,
+    propertyEditor: Editor
 }
 
 export default control
