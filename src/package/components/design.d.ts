96451da119f0bb374549d584e8a13788a5213669
xakoy <xakoy@qq.com>
2020-02-28 17:38:23 +0800

    设置types，增加智能提示

--- src/package/components/design.d.ts
+++ src/package/components/design.d.ts
@@ -1,11 +1,39 @@
 import { DesignModel } from '../interface'
 import { Component } from './component'
+import { CanMoveFunc } from './design-prop'
+import { IControl } from '../controls'
 
 export declare class Design extends Component {
     /**
      * 初始的JSON Model
      */
     defaultModel: DesignModel
+
+    /**
+     * 拖动控件默认的索引值
+     */
+    beginIndex: number
+
+    /**
+     * 布局控件空白时显示的文本
+     */
+    placeholder: string
+
+    /**
+     * 控件是否可以拖动
+     */
+    canMove: CanMoveFunc
+
+    /**
+     * 是否需要辅助显示线条
+     * @default false
+     */
+    isNeedSuportDisplay: boolean
+
+    /**
+     * 设计器所有控件集合，默认不用传值，当值不为空，则解析的控件始终在数组查找
+     */
+    allControls: IControl[]
 
     /**
      * 获取设计器的JSON Model
--- src/package/package.d.ts
+++ src/package/package.d.ts
@@ -1,4 +1,10 @@
-import Design from './components/design'
-import Viewer from './components/viewer'
+import Vue from 'vue'
+import { Design } from './components/design'
+import { Viewer } from './components/viewer'
+import * as Controls from './controls/control'
+import * as Properties from './controls/props'
+
+export function install(vue: typeof Vue)
 
 export { Design, Viewer }
+export { Controls, Properties }
