9b858361be9b297d3414ba8bebdcbf7aa72582d4
xakoy <xakoy@qq.com>
2020-04-22 11:06:13 +0800

    增加是否启用默认控件，和是否注册全局组件控件的选项

---
+++ src/views/components/complex/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <preview />
+    </div>
+</template>
+
+<script lang="ts">
+import Preview from '../../Preview.vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+    components: {
+        Preview
+    }
+})
+export default class Index extends Vue {}
+</script>
