6c3bfa2ca4858f604d093711af2f5f1dccce3f6b
xakoy <xakoy@qq.com>
2020-05-07 10:40:42 +0800

    Design组件，属性也能获取到extra

--- src/package/controls/PropertyEditItem.vue
+++ src/package/controls/PropertyEditItem.vue
@@ -1,18 +1,20 @@
 <template>
     <el-form-item v-if="rules" :rules="rules" :prop="property.propName">
-        <component :control="control" :property="property" :is="property.editor" />
+        <component :control="control" :property="property" :is="property.editor" :extra="extraInject" />
     </el-form-item>
-    <component v-else :control="control" :property="property" :is="property.editor" />
+    <component v-else :control="control" :property="property" :is="property.editor" :extra="extraInject" />
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop, Inject } from 'vue-property-decorator'
 import { IControl, IProperty } from '../interface'
+import { SYMBOL_EXTRA_KEY } from '../symbol'
 
 @Component
 export default class Index extends Vue {
     @Prop() control: IControl
     @Prop() property: IProperty
+    @Inject({ from: SYMBOL_EXTRA_KEY, default: {} }) extraInject: any
 
     get rules() {
         return this.property.rules
